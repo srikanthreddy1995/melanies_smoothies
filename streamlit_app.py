c1d44a335471198580034d54b448233dcf77c0e2
srikanthreddy1995 <srikanthdasari721@gmail.com>
2025-04-20 16:24:44 +0530

    Update streamlit_app.py
    up

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ name_on_order=st.text_input('name on smoo')
 st.write ('name on s will be:',name_on_order )
 cnx=st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 #st.stop()
 
